e65a4a503a567a6862b963c039e218f1b2bdc666
razbi <razhaim101@gmail.com>
2023-01-09 22:02:46 +0200

    models

--- Media_App/models.py
+++ Media_App/models.py
@@ -6,7 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+#RazModels
 
 class Households(models.Model):
     hid = models.IntegerField(db_column='hID', primary_key=True)  # Field name made lowercase.
